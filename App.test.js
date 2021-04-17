afe204a9fbef2757382a76479a3ba0f3064b11a2
JCOlazo1 <jay.olazo@gmail.com>
2021-04-17 01:25:15 -0400

    Add files via upload
    Added App.test.js with test functionalities

--- App.test.js
+++ App.test.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { unmountComponentAtNode } from 'react-dom';
 
-test('renders without crashing', () => {
-  render(<App />);
-  const checkContent = screen.getByText(/Grid/i);
-  expect(checkContent).toBeInTheDocument();
+import App from './App'
+import GifContainer from './components/gif-container/gif-container';
+
+let root = null;
+
+// Setup:
+beforeEach(() => {
+  root = document.createElement("div");
 });
+
+// Run tests HERE:
+
+test("renders, like at all", () => {
+  // Renders a React component to DOM:
+  // root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+
+  // Use DOM APIs (querySelector) to make assertions:
+  expect(root.querySelector("h1").textContent).toBe("Grid-splosion!");
+  expect(root.querySelector("button").textContent).toBe("SHOW THE GIFS!");
+});
+
+
+ // Cleanup:
+afterEach(() => {
+  unmountComponentAtNode(root);
+  root.remove();
+  root = null;
+})
